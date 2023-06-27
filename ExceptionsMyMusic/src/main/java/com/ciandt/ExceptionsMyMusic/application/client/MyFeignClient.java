7adc49ea373994f51089a19be2c183dca5ee8af2
Maikon Jakson Francelino Soares <jakson.pb11@gmail.com>
2023-06-27 19:47:00 -0300

    Update MyFeignClient.java

--- ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/application/client/MyFeignClient.java
+++ ExceptionsMyMusic/src/main/java/com/ciandt/ExceptionsMyMusic/application/client/MyFeignClient.java
@@ -1,5 +1,4 @@
 package com.ciandt.ExceptionsMyMusic.application.client;
-
 import com.ciandt.ExceptionsMyMusic.domain.dto.TokenDataDTO;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
